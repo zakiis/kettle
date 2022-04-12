142eae9d84b6f9fcd88ce44aad37e3e5982bc8a3
Liu Zhenghua <1090179900@qq.com>
2022-04-12 11:37:13 +0800

    add kettle service

--- src/main/java/com/zakiis/kettle/service/KettleService.java
+++ src/main/java/com/zakiis/kettle/service/KettleService.java
@@ -1,29 +1,71 @@
 package com.zakiis.kettle.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.repository.RepositoryDirectoryInterface;
 import org.pentaho.di.repository.kdr.KettleDatabaseRepository;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.steps.csvinput.CsvInputMeta;
+import org.pentaho.di.trans.steps.textfileinput.TextFileInputField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@SuppressWarnings("deprecation")
 public class KettleService {
 	
 	@Autowired
 	KettleDatabaseRepository repository;
 	
-	public void runTransfer(String transformName) throws KettleException {
-		RepositoryDirectoryInterface directoryTree = repository.loadRepositoryDirectoryTree();
+	Logger log = LoggerFactory.getLogger(KettleService.class);
+	
+	public boolean runTransform(String directory, String transformName, Map<String, String> params) throws KettleException {
+		RepositoryDirectoryInterface directoryTree = repository.findDirectory(directory);
 		TransMeta transMeta = repository.loadTransformation(transformName, directoryTree, null, true, null);
-		Trans trans=new Trans(transMeta); 
-        trans.execute(null); 
+		Trans trans=new Trans(transMeta);
+		if (params != null) {
+			for (Map.Entry<String, String> param: params.entrySet()) {
+				trans.setVariable(param.getKey(), param.getValue());
+			}
+		}
+		if (params.containsKey("inputFile")) {
+			setCsvInputFileds(transMeta, params.get("inputFile"));
+		}
+        trans.execute(null);
         trans.waitUntilFinished();//等待直到数据结束 
-        if(trans.getErrors()>0){ 
-            System.out.println("transformation error"); 
+        if(trans.getErrors()>0){
+        	log.error("transformation {} got some error", transformName, trans.getErrors());
+            return false;
         }else{ 
-            System.out.println("transformation successfully"); 
+            return true; 
         } 
+	}
+	
+	private void setCsvInputFileds(TransMeta transMeta, String csvFileName) throws KettleException {
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFileName))) {
+			String line = null;
+			if ((line = br.readLine()) != null) {
+				String[] columnNames = line.split(",");
+				TextFileInputField[] inputFields = new TextFileInputField[columnNames.length];
+				for (int i = 0; i < columnNames.length; i++) {
+					String columnName = columnNames[i];
+					if (columnName.startsWith("\"")) {
+						columnName = columnName.substring(1, columnName.length() - 1);
+					}
+					TextFileInputField inputField = new TextFileInputField(columnName, i, 0);
+					inputFields[i] = inputField;
+				}
+				((CsvInputMeta)transMeta.getStep(0).getStepMetaInterface()).setInputFields(inputFields);
+			}
+		} catch (Exception e) {
+			throw new KettleException("Analysis CSV header fields got an exception", e);
+		}
+		//(CsvInputMeta)transMeta.getStep(0).getStepMetaInterface()).setInputFields();
 	}
 }
--- src/test/java/com/zakiis/kettle/test/KettleServiceTest.java
+++ src/test/java/com/zakiis/kettle/test/KettleServiceTest.java
@@ -1,5 +1,10 @@
 package com.zakiis.kettle.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.pentaho.di.core.exception.KettleException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +19,67 @@ public class KettleServiceTest {
 	private KettleService kettleService;
 	
 	@Test
-	public void test() throws KettleException {
-		String transformName = "user";
-		kettleService.runTransfer(transformName);
+	public void testArchive() throws KettleException {
+		String directory = "trans";
+		String transformName = "archive";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("host", "192.168.137.105");
+		params.put("port", "3306");
+		params.put("name", "zakiis");
+		params.put("username", "root");
+		params.put("password", "123456");
+		params.put("sql", "select * from user");
+		params.put("outputFile", "D:\\data\\kettle\\user-20220411");
+		boolean result = kettleService.runTransform(directory, transformName, params);
+		assertEquals(result, true);
+	}
+	
+	@Test
+	public void testPurge() throws KettleException {
+		String directory = "trans";
+		String transformName = "purge";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("host", "192.168.137.105");
+		params.put("port", "3306");
+		params.put("name", "zakiis");
+		params.put("username", "root");
+		params.put("password", "123456");
+		params.put("sql", "delete from user where id < 10");
+		boolean result = kettleService.runTransform(directory, transformName, params);
+		assertEquals(result, true);
+	}
+	
+	@Test
+	public void testRestore() throws KettleException, InterruptedException {
+		String directory = "trans";
+		String transformName = "restore";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("host", "192.168.137.105");
+		params.put("port", "3306");
+		params.put("name", "zakiis");
+		params.put("username", "root");
+		params.put("password", "123456");
+		
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				params.put("inputFile", "D:\\data\\kettle\\address-20220411.csv");
+				params.put("table", "address");
+				try {
+					kettleService.runTransform(directory, transformName, params);
+				} catch (KettleException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+		thread.join();
+		
+		params.put("inputFile", "D:\\data\\kettle\\user-20220411.csv");
+		params.put("table", "user");
+		boolean result = kettleService.runTransform(directory, transformName, params);
+		assertEquals(result, true);
 	}
 }
